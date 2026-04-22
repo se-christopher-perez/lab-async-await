5b0f44cbb9a880e373602e18847dd23fb105b669
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-04-21 22:26:05 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your code here!
+
+const api = "https://jsonplaceholder.typicode.com/posts"
+
+function displayPosts(data){
+
+    const ul = document.querySelector("#post-list")
+
+    data.forEach(element => {
+
+        const h1 = document.createElement("h1")
+        const li = document.createElement("li")
+        const p = document.createElement("p")
+
+        h1.textContent = element["title"]
+
+        p.textContent = element["body"]
+
+        li.append(h1, p)
+
+        ul.append(li)
+
+    })
+
+}
+
+function fetchAPI(){
+
+    fetch(api)
+
+    .then(response => response.json())
+
+    .then((json) => {
+
+        const data = json
+
+        displayPosts(data)
+
+    })
+
+    .catch((error) => {
+
+        console.error(error)
+
+    })
+
+}
+
+fetchAPI()
